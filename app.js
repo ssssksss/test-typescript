ae578b33263114720cd7196a2c403fbdb9eb6bda
L <ssssksss@naver.com>
2021-04-29 23:52:53 +0900

    typescript first_test_file
    타입스크립트 간만보기

---
+++ app.js
@@ -0,0 +1,5 @@
+function logName(name) {
+    console.log(name);
+}
+logName('Hello');
+//app.ts 타이핑
---
+++ app.ts
@@ -0,0 +1,14 @@
+function logName(name: string) {
+    console.log(name);
+}
+
+logName('haha');
+//app.ts 타이핑
+
+// let student = {
+//     name: 'jake',
+//     course: '',
+//     code: function() {
+//         console.log('asd');
+//     }
+// }
